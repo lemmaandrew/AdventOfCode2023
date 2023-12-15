4955dbbf7ba2d78ce130b76d4ab24e3965f73a09
lemmaandrew <lemmajandrew@gmail.com>
2023-12-15 12:01:39 -0600

    Added proper command line arguments

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,39 @@
 module Main where
 
-import Day01              ( day01 )
-import System.Environment ( getArgs )
+import Day01               ( day01p1, day01p2 )
+import Options.Applicative
+    ( Parser
+    , auto
+    , execParser
+    , fullDesc
+    , help
+    , helper
+    , info
+    , long
+    , option
+    , short
+    , (<**>)
+    )
 
-days :: [IO ()]
-days = [day01]
+days :: [[IO ()]]
+days = [[day01p1, day01p2]]
+
+data Args =
+    Args
+        { day  :: Int
+        , part :: Int
+        }
+    deriving (Eq, Show)
+
+argsParser :: Parser Args
+argsParser =
+    Args
+    <$> option auto (long "day" <> short 'd' <> help "Which day to run")
+    <*> option auto (long "part" <> short 'p' <> help "Which part of the day to run")
 
 main :: IO ()
 main = do
-    [day] <- getArgs
-    days !! (read day - 1)
+    Args d p <- execParser opts
+    days !! (d - 1) !! (p - 1)
+  where
+    opts = info (argsParser <**> helper) fullDesc
