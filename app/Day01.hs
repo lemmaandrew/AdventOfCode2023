8aa06e3d9d7641d26855e89bdc2f316d77fa09a1
lemmaandrew <lemmajandrew@gmail.com>
2023-12-15 12:02:00 -0600

    Added day 1 part 2

--- app/Day01.hs
+++ app/Day01.hs
@@ -1,15 +1,43 @@
 module Day01
-    ( day01
+    ( day01p1
+    , day01p2
     ) where
 
-import Data.Char ( isDigit )
+import Data.Char ( digitToInt, isDigit )
 
 getCalibrationValue :: String -> Int
 getCalibrationValue line = read [head digits, last digits]
   where
     digits = filter isDigit line
 
-day01 :: IO ()
-day01 =
-    print =<<
-    sum . map getCalibrationValue . lines <$> readFile "data/day01.txt"
+day01p1 :: IO ()
+day01p1 =
+    print
+    =<< sum
+    . map getCalibrationValue
+    . lines
+    <$> readFile "data/day01.txt"
+
+day01p2 :: IO ()
+day01p2 = sum <$> calibrationValues >>= print
+  where
+    fileLines = lines <$> readFile "data/day01.txt"
+
+    -- parses all the digits of a line, including the words
+    parseLine [] = []
+    parseLine ('o':'n':'e':xs) = 1 : parseLine ('e' : xs)
+    parseLine ('t':'w':'o':xs) = 2 : parseLine ('o' : xs)
+    parseLine ('t':'h':'r':'e':'e':xs) = 3 : parseLine ('e' : xs)
+    parseLine ('f':'o':'u':'r':xs) = 4 : parseLine xs
+    parseLine ('f':'i':'v':'e':xs) = 5 : parseLine ('e' : xs)
+    parseLine ('s':'i':'x':xs) = 6 : parseLine xs
+    parseLine ('s':'e':'v':'e':'n':xs) = 7 : parseLine ('n' : xs)
+    parseLine ('e':'i':'g':'h':'t':xs) = 8 : parseLine ('t' : xs)
+    parseLine ('n':'i':'n':'e':xs) = 9 : parseLine ('e' : xs)
+    parseLine (x:xs)
+        | isDigit x = digitToInt x : parseLine xs
+        | otherwise = parseLine xs
+
+    parsedLines = map parseLine <$> fileLines
+
+    calibrationValues = map (\line -> head line * 10 + last line) <$> parsedLines
